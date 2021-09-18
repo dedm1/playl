7c45fa7630a32779e2deb273daa4863143488eb4
danil.plaksin1@gmail.com <danil.plaksin1@gmail.com>
2021-09-18 22:42:44 +0700

    bar

--- widget.cpp
+++ widget.cpp
@@ -21,6 +21,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 using namespace std;
 
 /*function... might want it in some class?*/
@@ -28,12 +29,16 @@ using namespace std;
 
 QDir fileName1;
 QDir fileName2;
+bool flag = false;
+bool flag1= false;
+bool flag2 = false;
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    ui->progressBar->setVisible(false);
 }
 
 Widget::~Widget()
@@ -43,36 +48,43 @@ Widget::~Widget()
 
 void Widget::on_pushButton_clicked()
 {
+    flag1= true;
      fileName1 = QFileDialog::getExistingDirectory(this, tr("откуда"), "");
-
      ui->label->setText(fileName1.dirName());
 }
 
 
 void Widget::on_pushButton_2_clicked()
 {
+    flag2= true;
     fileName2 = QFileDialog::getExistingDirectory(this, tr("куда"), "");
-   // QString x=fileName2;
    ui->label_2->setText(fileName2.dirName());
 }
 
-void perzapis(QFileInfoList list )
-{
-
-}
 void Widget::on_pushButton_3_clicked()
 {
-    int  file1;
+    int  file1=0;
     QString x= ui->lineEdit->text();
     bool ok = false;
+    bool ok1 = true;
+    int numder=0;
       file1 = x.toInt(&ok, 10);
-      if (!ok) {
-          ui->label_3->setText("Couldn't get a number from input!!!\n");
-          return;
-      }
-     //консольное приложение
           QStringList nameFilter;
           nameFilter << "*.mp3" ;
+          if (!flag1 )
+          {
+            ui->label_3->setText("нажмите на кнопку выбрать папку\n");
+            return;
+          }
+          if (!flag2)
+          {
+            ui->label_3->setText("нажмите на кнопку скопировать в\n");
+            return;
+          }
+          if (!ok) {
+              ui->label_3->setText("введите число для копирования\n");
+              return;
+          }
           fileName1.setFilter(QDir::Files | QDir::Hidden | QDir::NoSymLinks);   ///устанавливаем фильтр выводимых файлов/папок (см ниже)
           fileName1.setSorting(QDir::Size | QDir::Reversed);   //устанавливаем сортировку "от меньшего к большему"
           QFileInfoList list = fileName1.entryInfoList(nameFilter,QDir::Files); //получаем список файлов директории
@@ -100,12 +112,53 @@ std::cout << std::endl;
                   for (int i = 0; i < file1; i++) {
                       files_list += list[i].absoluteFilePath() + "\n";
                   }
-    ui->label_3->setText(files_list);
-    for (int i = 0; i < file1; i++) {
+    //ui->label_3->setText(files_list);
+      ui->progressBar->setVisible(true);
+      ui->label_3->setText("копирование началось \n");
+      //ui->progressBar->setValue(15);
+    if (flag==false){
+        for (int i = 0; i < file1; i++) {
+            ui->progressBar->setValue( ((double) i/ (double) file1)*100 );
         if (!QFile::copy(list[i].absoluteFilePath(), fileName2.absolutePath() + '/' + list[i].fileName())) {
             std::cerr << "Couldn't copy file!!!\n";
-            ui->label_3->setText("Couldn't copy file!!!\n");
+            ui->label_3->setText("Не удалось скопировать файл!!!\n");
+            ok1=false;
             break;
         }
+            numder=i;
+        }
+    }
+    if (flag==true)
+    {
+        for (int i = 0; i < file1; i++) {
+            ui->progressBar->setValue( ((double) i/ (double) file1)*100 );
+            QString name1=QString::number(i);
+            if (!QFile::copy(list[i].absoluteFilePath(), fileName2.absolutePath() + '/' +name1+ list[i].fileName())) {
+                std::cerr << "Couldn't copy file!!!\n";
+                ok1=false;
+                ui->label_3->setText("Не удалось скопировать файл!!!\n");
+                break;
+            }
+            numder=i;
+        }
+    }
+    if (ok1){    ui->progressBar->setValue(100);
+    ui->label_3->setText("копирование завершено, файлов скопиравнно: \n");
+    ui->label_5->setText( QString::number(numder));
+    }
+}
+
+
+
+
+void Widget::on_checkBox_clicked(bool checked)
+{
+    if (checked==false)
+    {
+        flag = false;
+    }
+    if (checked==true)
+    {
+        flag = true;
     }
 }
--- widget.h
+++ widget.h
@@ -20,6 +20,9 @@ private slots:
     void on_pushButton_2_clicked();
 
     void on_pushButton_3_clicked();
+
+    void on_checkBox_clicked(bool checked);
+
 private:
     Ui::Widget *ui;
 };
